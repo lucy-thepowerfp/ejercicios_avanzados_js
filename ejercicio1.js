8565114bbefcb45511dec0988e46646ca3c47e8a
lucy <lucyhapsti@gmail.com>
2025-07-28 20:13:59 +0200

    Initial commit

---
+++ ejercicio1.js
@@ -0,0 +1,34 @@
+const movies = [
+    {
+      title: "Bracula: Condemor II",
+      duration: 192,
+      categories: ["comedia", "aventura"],
+    },
+    {
+      title: "Spider-Man: No Way Home",
+      duration: 122,
+      categories: ["aventura", "acción"],
+    },
+    {
+      title: "The Voices",
+      duration: 223,
+      categories: ["comedia", "thriller"],
+    },
+    {
+      title: "Shrek",
+      duration: 111,
+      categories: ["comedia", "aventura", "animación"],
+    },
+];
+
+let categories = [];
+
+for (movie of movies){
+    for (category of movie.categories){
+        if (!categories.includes(category)){
+            categories.push(category);
+        }
+    }
+}
+
+console.log(categories);
